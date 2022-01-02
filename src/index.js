eb437a55546d170684d5506adcc1dfd70ec6f4dd
oliverlevay <oliver.levay@hotmail.com>
2022-01-03 00:29:13 +0100

    Enable introspection

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const server = new ApolloServer({
   resolvers,
   typeDefs,
   plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+  introspection: true,
 });
 
 server.listen({ port }, () =>
