92a8a5aa2ec40ff4b6547d3e0313b66406ad952d
oliverlevay <oliver.levay@hotmail.com>
2022-01-03 00:26:18 +0100

    Add playground

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 const { ApolloServer } = require('apollo-server');
+const {
+  ApolloServerPluginLandingPageGraphQLPlayground,
+} = require('apollo-server-core');
 const { typeDefs } = require('./schema');
 const { resolvers } = require('./resolvers');
 
 const port = process.env.PORT || 9090;
 
-const server = new ApolloServer({ resolvers, typeDefs });
+const server = new ApolloServer({
+  resolvers,
+  typeDefs,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+});
 
 server.listen({ port }, () =>
   console.log(`Server runs at: http://localhost:${port}`)
